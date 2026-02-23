7d432d483072a2a8e88cea37688f696c59eff09a
june <june@fake.com>
2026-02-23 14:58:51 -0500

    new sprite system good

--- sprite.lua
+++ sprite.lua
@@ -1,136 +1,69 @@
 transform = {}
 transform.__index = transform
-function transform:new(x, y, r, sx, sy, ox, oy)
-    local o = setmetatable({}, self)
-    o.x = x or 0
-    o.y = y or 0
-    o.r = r or 0
-    o.sx = sx or 1
-    o.sy = sy or sx or 1
-    o.ox = ox or 0
-    o.oy = oy or 0
-
-    return o
-end
-
-function transform:unpack()
-    return self.x, self.y, self.r, self.sx, self.sy, self.ox, self.oy
+-- dx, dy -> translation
+-- dr     -> rotation (radians)
+-- sx, sy -> scale factor
+-- ox, oy -> offset
+-- maybe TODO: kx, ky -> shear factor (idk how this works & havent needed it)
+function transform:new(dx, dy, r, sx, sy, ox, oy)
+	local o = setmetatable({}, self)
+	o.dx = dx or 0
+	o.dy = dy or 0
+	o.dr = dr or 0
+	o.sx = sx or 1
+	o.sy = sy or sx or 1
+	o.ox = 0
+	o.oy = 0
+	-- kx
+	-- ky
+	return o
 end
 
 function transform:compound(other)
-    c.x = self.x + other.x
-    c.y = self.y + other.y
-    c.r = self.r + other.r
-    c.sx = self.sx * other.sx
-    c.sy = self.sy * other.sy
-    c.ox = self.ox + other.ox
-    c.oy = self.oy + other.oy
+	self.x = self.x + other.x
+	self.y = self.y + other.y
+	self.r = self.r + other.r
+	self.sx = self.sx * other.sx
+	self.sy = self.sy * other.sy
+	self.ox = self.ox + other.ox
+	self.oy = self.oy + other.oy
+	-- kx
+	-- ky
+	return self -- for chaining
 end
 
-nulltransform = transform:new()
-nulltransform.init = nil
-
-
-actor = {}
-actor.__index = actor
--- is created with no animation data, just a default costume & transform
-function actor:new(imgpath, inittransform)
-    local o = setmetatable({}, self)
-    o.defaultcostume = stripfilename(imgpath)
-    o.costume = stripfilename(imgpath)
-    o.costumes = {}
-    o.costumes[o.costume] = love.graphics.newImage(imgpath)
-
-    o.transform = copy(inittransform) or copy(nulltransform)
-    o.transform.init = inittransform
-    
-    o.animations = {}
-    o.animation = 0
-    o.frame = 0
-
-    o.x = 0
-    o.y = 0
-    o.r = 0
-    o.sx = 1
-    o.sy = 1
-    o.ox = 0
-    o.oy = 0
-    return o
+function transform:unpack()
+	return self.x, self.y, self.r, self.sx, self.sy, self.ox, self.oy -- self.kx, self.ky
 end
 
-function actor:draw()
-    love.graphics.draw(self.costumes[self.costume], self.transform:unpack())
-end
 
-function actor:addanimation(name, framedata)
-    self.animations[name] = {}
-    local tmp = {}
-    for _, f in ipairs(framedata) do
-        if type(f) == "number" or type(r) == "function" and #tmp > 0 then
-            table.insert(self.animations[name], frame:new(table.unpack(tmp)))
-            tmp = {}
-        end
-        table.insert(tmp, f)
-    end    
-end
-
-function actor:freeze(costume, transform)
-    self.animation = 0
-    self.frame = 0
-    self.costume = costume or self.defaultcostume
-    self.transform = transform or self.transform.init
-    return
-end
-    
-function actor:update(dt, ctx)
-    if #animations == 0 then return end
-    local currentanim = self.animations[self.animation]
-    if not currentanim then return end
-    local currentframe = currentanim.frames[self.currentframe]
-    if not currentframe then return end
-    
-    local passed, amt = false, 0
-    
-    if currentframe.test then
-        passed, amt = currentframe:test(dt, ctx)
-        if not amt then amt = 1 end
-    elseif currentframe.timer then
-        if currentframe.timer:countdown(dt) then
-            currentframe.timer:reset()
-            passed, amt = true, 1
-        end
-    else passed, amt = true, 1 end
-    
-    if passed then
-        if self.currentframe > #currentanim and not currentanim.loops then
-            local freezeon = {self.defaultcostume, self.transform.init or nulltransform}
-            if currentanim.freezeon > 0 then
-            	for _, f in ipairs(currentanim.frames) do
-					freezeon[2]:compound(f.transform)
-            	end
-                local freezeframe = currentanim.frames[currentanim.freezeon]
-                freezeon = {freezeframe.costume, freezeframe.transform}
-            end
-            self:freeze(table.unpack(freezeon))
-        end
-    
-        if currentanim.loops then
-            self.currentframe = self.currentframe % #currentanim + 1
-        end
-    end
+frame = {}
+frame.__index = frame
+-- time      -> number (seconds)
+-- test      -> function(actor, dt, ctx): bool, number?
+-- costume   -> string (id which is the image's filename w/o extension)
+-- transform -> transform
+function frame:new(...)
+	local o = setmetatable({}, self)
+	local args = {...}
+	for _, arg in ipairs(args) do
+		if type(arg) == "number" then o.timer = timer:new(arg) end
+		if type(arg) == "function" then o.test = arg end
+		if type(arg) == "string" then o.costume = arg end
+		if getmetatable(arg) == transform then o.transform = arg end
+	end
+	
+	return o
 end
 
 
 animation = {}
 animation.__index = animation
--- framedata -> [][]frame:new args
+-- framedata -> args for frame constructor
 -- loops     -> bool
--- freezeon  -> framedata index
--- (see docs for 'frame')
-function animation:new(framedata, loops, freezeon)
-    local o = setmetatable(o, self)
+function animation:new(framedata, loops)
+	local o = setmetatable({}, self)
     o.loops = loops
-    o.freezeon = freezeon or 0
     
     o.frames = {}
     for _, args in ipairs(framedata) do
@@ -140,24 +73,104 @@ function animation:new(framedata, loops, freezeon)
     return o
 end
 
-frame = {}
-frame.__index = frame
--- time      -> number (seconds)
--- test      -> function(self, dt, ctx): bool, number?
--- costume   -> string (id which is the image's filename w/o extension)
--- transform -> love.math.transform object (for rotation, scaling, etc)
-function frame:new(...)
-    local o = setmetatable({}, self)
-    
-    local arg = {...}
-    for i, v in ipairs(arg) do
-        if i > 4 then break end
-        if     type(v) == "number"   then o.timer = timer:new(v)
-        elseif type(v) == "function" then o.test = v
-        elseif type(v) == "string"   then o.imgpath = v
-        elseif v:typeOf("Transform") then o.transform = v 
-        end
-    end
-    
-    return o    
+-- upto -> number (compound transforms up to this index, inclusive)
+function animation:compoundtransforms(upto)
+	if upto > #self.frames and not self.loops then upto = #self.frames end
+
+	local t = transform:new()
+	for i = 0, upto - 1 do
+		local f = self.frames[i % #self.frames + 1]
+		if f.transform then t:compound(f.transform) end
+	end
+	
+	return t
+end
+
+
+actor = {}
+actor.__index = actor
+function actor:new(basepath, defcostume, deftransform)
+	local o = setmetatable({}, self)
+	o.costume = defcostume
+	o.costumes = {}
+	o.costumes[defcostume] = love.graphics.newImage(basepath..defcostume..".png")
+	o.costumes.init = defcostume
+
+	o.transform = deftransform
+	o.transform.init = deftransform
+
+	o.animations = {}
+	o.frozen = false
+	o.animation = nil
+	o.frame = nil
+
+	return o
+end
+
+function actor:draw()
+	love.graphics.draw(self.costumes[self.costume], self.transform:unpack())
+end
+
+-- function actor:addanimation(name, anim)
+-- 	self.animations[name] = anim
+-- end
+
+-- animname -> string
+-- frameidx -> number or nil
+-- MUST BE CALLED WITH UPDATE TO WORK !!
+function actor:startanimation(animname, frameidx)
+	self.animation = animname
+	-- don't allow 0, which means animation is frozen
+	self.frame = math.min(frameidx, 1) or 1
+	-- return self -- for chaining
+end
+
+function actor:stopanimation()
+	self.animation = nil
+	self.frame = nil
+	self.costume = self.costumes.init
+end
+
+-- animname -> string
+-- frameidx -> number or nil
+-- function actor:freeze(animname, frameidx)
+-- 	self:setanimation(animname, frameidx)
+-- 	self.frozen = true -- blocks update
+-- end
+
+-- dt  -> number
+-- ctx -> table with anything you want
+function actor:update(dt, ctx)
+	if #animations == 0 or self.frozen then return end
+	local currentanim = self.animations[self.animation]
+	if not currentanim then return end
+
+	local currentframeidx = (self.frame-1) % #self.frames + 1
+	if self.frame ~= currentframeidx and not currentanim.loops then
+		self.frame = #currentanim.frames
+		return
+	end
+	
+	local currentframe = currentanim.frames[currentframeidx]
+
+	local passed, amt = false, 0
+	if currentframe.timer then
+		if currentframe.timer:countdown(dt) then
+			passed, amt = true, 1
+		end
+	elseif currentframe.test then
+		passed, amt = currentframe.test(self, dt, ctx)
+	end
+
+	if passed then
+		local noloop = self.frame + amt
+		self.frame = (noloop-1) % #self.frames + 1
+		if self.frame ~= noloop and not currentanim.loops then
+			self.frame = #self.frames
+		end
+		currentframe = self.frames[self.frame]
+		
+		if currentframe.costume then self.costume = currentframe.costume end
+		if currentframe.transform then self.transform:compound(currentframe.transform) end
+	end
 end
